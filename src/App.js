ef528a447070c41ff60f319fa27967b7e0a5e413
mook1954 <wjdanr89@gmail.com>
2021-01-29 15:47:16 +0900

    챕터6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
 import React, { Component } from "react";
 import ScrollBox from "./chapter5/ScrollBox";
+import IterationSample from "./chapter6/IterationSample";
 import { render } from "@testing-library/react";
 
 class App extends Component {
   render() {
     return (
       <div>
+        <IterationSample></IterationSample>
+        {/* 
         <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨밑으로
-        </button>
+          <button onClick={() => this.scrollBox.scrollToBottom()}>
+            맨밑으로
+          </button> 
+        */}
       </div>
     );
   }
